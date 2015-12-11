2833535cccda446c54e9099e702fed99855b68d4
Tyler jarrard <tjar2074@740s-pl-104.local>
2015-12-11 09:54:33 -0700

    ugly layout

---
+++ stupidIOSProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stupidIOSProject
+//
+//  Created by Jarrard, Tyler on 12/11/15.
+//  Copyright (c) 2015 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
