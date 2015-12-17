43ec33814cb6137c509bc89326993ae7dacfa3b8
Tyler jarrard <tjar2074@740s-pl-104.local>
2015-12-17 09:08:21 -0700

    color switches

--- stupidIOSProject/ViewController.swift
+++ stupidIOSProject/ViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
+    @IBAction func changeColor(sender: UIButton)
+    {
+        randomColorChange()
+    }
+    
+    func randomColorChange()
+    {
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        let newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +35,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func Red(sender: UISwitch)
+    {
+        if(sender.on)
+        {
+            view.backgroundColor = UIColor.redColor()
+        }
+        else
+        {
+            view.backgroundColor = UIColor.whiteColor()
+        }
+    }
 
+    @IBAction func blue(sender: UISwitch)
+    {
+        if(sender.on)
+        {
+            view.backgroundColor = UIColor.blueColor()
+        }
+        else
+        {
+            view.backgroundColor = UIColor.whiteColor()
+        }
+    }
+    @IBAction func green(sender: UISwitch)
+    {
+        if(sender.on)
+        {
+            view.backgroundColor = UIColor.greenColor()
+        }
+        else
+        {
+            view.backgroundColor = UIColor.whiteColor()
+        }
+
+    }
+    
 }
 
